a40a71d74b0d82d647eaa94ce43861c0dfc24747
alvin870203 <alvin870203@gmail.com>
2022-01-28 23:57:50 +0800

    first commit

---
+++ app/src/main/java/org/pytorch/emotion/EmotionClasses.java
@@ -0,0 +1,13 @@
+package org.pytorch.emotion;
+
+public class EmotionClasses {
+    public static String[] EMOTION_CLASSES = new String[]{
+        "Angry",
+        "Disgust",
+        "Fear",
+        "Happy",
+        "Neutral",
+        "Sad",
+        "Surprise"
+    };
+}
